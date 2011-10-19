2b62eda4f13cc6409bc3c7e6df42584b2d684371
Nerijus Arlauskas <nercury@gmail.com>
2011-10-19 19:18:55 +0300

    Sdl Window Info.
    Signed-off-by: Nerijus Arlauskas <nercury@gmail.com>

--- key-opengl/SDLWindowInfo.cpp
+++ key-opengl/SDLWindowInfo.cpp
@@ -28,6 +28,8 @@ void SDLWindowInfo::render()
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	glViewport(10, 10, 110, 110);
+
 	glBegin(GL_QUADS);
         glColor3f(1, 0, 0); glVertex3f(0, 0, 0);
         glColor3f(1, 1, 0); glVertex3f(100, 0, 0);
